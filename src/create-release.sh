cc520ad55b12ef002de710cf7251fc6edb93c5e1
Riki Nakamura <riki.nakamura.edu@gmail.com>
2025-07-22 13:39:26 +0900

    feat(create-release): add logging

--- src/create-release.sh
+++ src/create-release.sh
@@ -20,5 +20,8 @@ if [ -z "$NEW_VERSION_TAG" ]; then
 fi
 
 # Create GitHub Release
+echo "Creating GitHub Release for tag: ${NEW_VERSION_TAG}"
 # --generate-notes will automatically generate release notes based on git history
 gh release create "$NEW_VERSION_TAG" --target "$MERGE_COMMIT_SHA" --generate-notes
+
+echo "GitHub Release created successfully."
