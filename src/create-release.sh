0c8c8a2f0b7ff547100069ce2d5df9841d5842b0
Riki Nakamura <riki.nakamura.edu@gmail.com>
2025-07-22 13:43:13 +0900

    refactor(create-release): remove unnecessary checks

--- src/create-release.sh
+++ src/create-release.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -e
 
-# Ensure GH_TOKEN is set
-if [ -z "$GH_TOKEN" ]; then
-    echo "GH_TOKEN is not set. Exiting."
-    exit 1
-fi
-
-# Ensure MERGE_COMMIT_SHA is set
-if [ -z "$MERGE_COMMIT_SHA" ]; then
-    echo "MERGE_COMMIT_SHA is not set. Exiting."
-    exit 1
-fi
-
 # Verify if new version tag is set
 if [ -z "$NEW_VERSION_TAG" ]; then
     echo "New version tag not found. Skipping."
